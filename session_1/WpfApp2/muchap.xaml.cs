e2dbf08617f92bc889c5ded14e3613936b0f2714
vlad <evo.174@yandex.ru>
2019-06-03 18:37:34 +0300

    first commit

---
+++ session_1/WpfApp2/muchap.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для muchap.xaml
+    /// </summary>
+    public partial class muchap : Window
+    {
+        public muchap()
+        {
+            InitializeComponent();
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
